17de4c286be8317f3964b62201f3c1478d415216
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2024-09-18 17:18:02 +0500

    javascript darslar

---
+++ index.js
@@ -0,0 +1,54 @@
+
+///////////////// 1-masala
+
+// function mathPowMetod(num1 , num2) {
+//   let res = 1
+//   for (let i = 0; i < num2; i++) {
+//     res *= num1
+    
+//   }
+//   return res
+// }
+
+// console.log(mathPowMetod(4 , 3));
+
+/////////////////// 2-masala
+
+// let arr = [11 , 6 , 6 , 4 , 90 , 10]
+// function mathMin(arr) {
+//   let res = Math.max(...arr)
+   
+//   for (let i = 0; i < arr.length; i++) {
+//     if (res > arr[i]) {
+//       res = arr[i]
+//     }
+    
+//   }
+//   return res
+// }
+
+// console.log(mathMin(arr));
+
+//////////////////// 3-masala
+
+// let arr = [11 , 6 , 6 , 4 , 90 , 10]
+// function mathMaxMetod(arr) {
+//   let res = 1
+//   for (let i = 0; i < arr.length; i++) {
+   
+//     if (arr[i] > res) {
+//      res = arr[i] 
+//     }
+    
+//   }
+//   return res
+// }
+// console.log(mathMaxMetod(arr));
+
+/////////////////////// 4-masala
+
+// function returnerFloor(num) {
+//   return Number(num)
+// }
+// console.log(returnerFloor("768.76"));
+
